0cfaab1222f6ac10b76f0dff3de9ef47815a0ea3
Veljko Loncarevic <veljkoloncarevicharry@gmail.com>
2023-10-03 18:52:45 +0200

    Changed position of my name in title

--- public/html/main.php
+++ public/html/main.php
@@ -9,7 +9,7 @@ if (isset($_GET["id"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Veljko Lončarević | <?= (isset($_GET["id"]) ? $work["title"] : "Literature"); ?></title>
+    <title><?= (isset($_GET["id"]) ? $work["title"] : "Literature"); ?> | Veljko Lončarević</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="public/styles/style.css"/>
 
