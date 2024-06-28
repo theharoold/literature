e75c1b64334ed8c1f139fc843200a67366972ae7
Veljko Loncarevic <veljkoloncarevicharry@gmail.com>
2024-06-28 19:38:03 +0200

    Updated header with age and master's degree

--- public/html/header.php
+++ public/html/header.php
@@ -1,18 +1,16 @@
         <header class="header">
             <div id="ime">
                 <h1>
-                    Veljko Lončarević, 23
+                    Veljko Lončarević, 24
                 </h1>
                 <p id="zvanje">
                     Data Scientist
                 </p>
                 <p id="opis">
-                    Master's student specializing in Information Technology at University of Kragujevac. 
+                    Master Engineer of Information Technology at University of Kragujevac. 
                     Working as a Data Scientist in the Business Intelligence Department for Brezna and Avnet EMEA. 
                     Certified in Machine Learning, Artificial Intelligence, and Databases by Microsoft, Oracle, Huawei, IBM, 
-                    Deeplearning.AI. Finished an internship for a Back-end developer. 
-                    <br><br>
-                    Held a tenure as a Student Assistant at the Faculty of Technical Sciences in Čačak. Currently engaged in cutting-edge 
+                    Deeplearning.AI. Finished an internship for a Back-end developer. Held a tenure as a Student Assistant at the Faculty of Technical Sciences in Čačak. Currently engaged in cutting-edge 
                     research within the field of Artificial Intelligence, driving innovation and advancing knowledge in this dynamic domain. 
                     Passionate about leveraging technology to solve complex challenges and drive organizational success.    
                 </p>
